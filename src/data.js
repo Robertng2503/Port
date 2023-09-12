6c5f3f2adb0e89bfa824a194a27c78db85aed5b0
Robert Nguyen <robertng.thien@gmail.com>
2023-09-12 12:26:53 -0700

    first commit

---
+++ src/data.js
@@ -0,0 +1,60 @@
+export const projects = [
+  {
+    title: "React Reserve",
+    subtitle: "MERN Stack",
+    description:
+      "Information about the porject",
+    image: "./project-1.gif",
+    link: "https://",
+  },
+  {
+    title: "Demo",
+    subtitle: "Demo",
+    description:
+      "Information about the porject",
+    image: "./project-2.gif",
+    link: "https://",
+  },
+  {
+    title: "Demo",
+    subtitle: "Demo ",
+    description:
+      "Information about the porject",
+    image: "./project-3.gif",
+    link: "https://",
+  },
+  {
+    title: "Demo",
+    subtitle: "DEmo",
+    description:
+      "Information about the porject",
+    image: "./project-4.gif",
+    link: "https://",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Html",
+  "Node",
+  "SQL",
+  "Css",
+];
